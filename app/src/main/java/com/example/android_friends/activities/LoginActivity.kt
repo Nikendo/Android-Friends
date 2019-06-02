98ddf2e1458be12cd977cf17f53b5775c292952b
Nikendo <n.shmatov@intecgroup.ru>
2019-06-03 01:53:32 +0700

    Building login activity

--- app/src/main/java/com/example/android_friends/activities/LoginActivity.kt
+++ app/src/main/java/com/example/android_friends/activities/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.example.android_friends
+package com.example.android_friends.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_friends.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
